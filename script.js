c0eabe0f095ecadd0672fd9a60a5d31ef8de1904
IndifferentSoul <whitecloudlurker@gmail.com>
2022-01-07 03:24:40 +0000

    Added a simple GUI Reworked how the javasript works. It now playable on a basic HTML website instead of the console.

--- script.js
+++ script.js
@@ -1,72 +1,120 @@
 const SHAPES = ["Rock", "Paper", "Scissors"];
-let playerWins = 0
-let computerWins = 0
+let playerWins = 0;
+let computerWins = 0;
+
+const playerscore = document.querySelector(".player-wins");
+const computerscore = document.querySelector(".computer-wins");
+const result = document.querySelector(".result");
+
+
+//Player plays rock
+const rock = document.querySelector("#rock");
+rock.addEventListener('click', function (e) {
+    playRound("Rock", computerPlay())
+    playerscore.textContent = `Player: ${playerWins}`;
+    computerscore.textContent = `Computer: ${computerWins}`;
+   
+    if (playerWins > 4) {
+        gameWin();
+    } else if (computerWins > 4) {
+        gameLoss();
+    }
+})
+
+//Player plays paper
+const paper = document.querySelector("#paper");
+paper.addEventListener('click', function (e) {
+    playRound("Paper", computerPlay())
+    playerscore.textContent = `Player: ${playerWins}`;
+    computerscore.textContent = `Computer: ${computerWins}`;
+
+    if (playerWins > 4) {
+        gameWin();
+    } else if (computerWins > 4) {
+        gameLoss();
+    }
+})
+
+//Player plays scissors
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener('click', function (e) {
+    playRound("Scissors", computerPlay())
+    playerscore.textContent = `Player: ${playerWins}`;
+    computerscore.textContent = `Computer: ${computerWins}`;
+
+    if (playerWins > 4) {
+        gameWin();
+    } else if (computerWins > 4) {
+        gameLoss();
+    }
+})
 
 //Get a random shape from the SHAPES array
 function computerPlay() {
     return SHAPES[Math.floor(Math.random() * SHAPES.length)];
 } 
 
-//Ask for users choice in shape and make it case-insensitive
-function playerPlay() {
-    let choice = prompt("Which shape(rock, paper or scissors) would you like to use?")
-    return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-}
-
-//Play one round of rock paper scissors and display the result in console
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log(`It's a tie you both played ${playerSelection}`)
+        result.textContent = `It's a tie you both played ${playerSelection}.`
     }
     if (playerSelection != computerSelection) {
         if (computerSelection === "Rock") {
             if (playerSelection === "Paper") {
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-                playerWins ++;
+                roundWin (playerSelection, computerSelection)
             }  else {
-                console.log(`You lose! ${playerSelection} doesn't beat ${computerSelection}`)
-                computerWins++;
+                roundLoss (playerSelection, computerSelection)
             }
         }
 
         if (computerSelection === "Paper") {
             if (playerSelection === "Scissors") {
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-                playerWins ++;
+                roundWin (playerSelection, computerSelection)
             } else {
-                console.log(`You lose! ${playerSelection} doesn't beat ${computerSelection}`)
-                computerWins++;
+                roundLoss (playerSelection, computerSelection)
             }
         }
 
         if (computerSelection === "Scissors") {
             if (playerSelection === "Rock") {
-                console.log(`You win! ${playerSelection} beats ${computerSelection}`)
-                playerWins ++;
+                roundWin (playerSelection, computerSelection)
             } else {
-                console.log(`You lose! ${playerSelection} doesn't beat ${computerSelection}`)
-                computerWins++;
+                roundLoss (playerSelection, computerSelection)
+
             }
         }
     }
 }
 
-function game() {
-    //Repeat the playRound function 5 times to stimulate 5 different Rock Paper Scissor games
-    for (let i = 1; i < 6; i++) {
-        console.log(`Round number: ${i}`)
-        playRound(playerPlay(), computerPlay())
-    }
-    
-    console.log(`You wins: ${playerWins} Computer wins: ${computerWins}`)
-    
-    if (playerWins === computerWins) {
-        console.log("It's a Tie")
-    } else if (playerWins > computerWins) {
-        console.log("Congratulations! You Won!")
-    } else if (playerWins < computerWins) {
-        console.log("You lost! Better luck next time.")
-    }
+function roundWin (playerSelection, computerSelection) {
+    result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`
+    playerWins ++;
 }
 
-game()
+function roundLoss (playerSelection, computerSelection) {
+    result.textContent = `You lose! ${playerSelection} doesn't beat ${computerSelection}.`
+    computerWins++;
+}
+
+function gameWin () {
+    alert("Congratulations! You Win!");
+    
+    playerWins = 0;
+    computerWins = 0;
+
+    playerscore.textContent = `Player: ${playerWins}`;
+    computerscore.textContent = `Computer: ${computerWins}`;
+    result.textContent = '';
+}
+
+function gameLoss () {
+    alert("The computer wins! Better luck next time!");
+    
+    playerWins = 0;
+    computerWins = 0;
+
+    playerscore.textContent = `Player: ${playerWins}`;
+    computerscore.textContent = `Computer: ${computerWins}`;
+    result.textContent = '';
+}
+
